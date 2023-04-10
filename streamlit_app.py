c15d562f12d82977c34a85213e94824ab52f4222
Akhtar <71397909+IamAKhtar@users.noreply.github.com>
2023-04-10 10:48:10 +0530

    Update streamlit_app.py
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
